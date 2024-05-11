ef4bd1eb266e265a05df0c83b0b72fafaae0ef1a
Maksym Naumchuk <vhemaster98@gmail.com>
2024-05-11 23:34:24 +0300

    Use CRC and DMA for write verification instead of readout and memory comparison

--- CM7/ECU/Config/Flash/Src/flash_fsm.c
+++ CM7/ECU/Config/Flash/Src/flash_fsm.c
@@ -16,7 +16,6 @@ error_t flash_fsm(flash_runtime_ctx_t *ctx)
   error_t err = E_OK;
   uint32_t insector_addr_mask;
   uint32_t address;
-  bool compare_success;
 
   while(true) {
     switch(ctx->fsm_process) {
@@ -330,37 +329,10 @@ error_t flash_fsm(flash_runtime_ctx_t *ctx)
       case FLASH_FSM_WRITE_HEADER_SYNC:
         err = qspi_sync(ctx->qspi_ctx);
         if(err == E_OK) {
-          ctx->fsm_process = FLASH_FSM_WRITE_HEADER_VERIFY;
-          continue;
-        } else if(err != E_AGAIN) {
-          ctx->cmd_errcode_internal = err;
-          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
-        }
-        break;
-      case FLASH_FSM_WRITE_HEADER_VERIFY:
-        err = qspi_fast_read(ctx->qspi_ctx, ctx->cmd_address, ctx->cmd_write_verify, ECU_FLASH_SECTION_HEADER_LENGTH);
-        if(err == E_OK) {
-          ctx->fsm_process = FLASH_FSM_WRITE_HEADER_VERIFY_SYNC;
-          continue;
-        } else if(err != E_AGAIN) {
-          ctx->cmd_errcode_internal = err;
-          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
-        }
-        break;
-      case FLASH_FSM_WRITE_HEADER_VERIFY_SYNC:
-        err = qspi_sync(ctx->qspi_ctx);
-        if(err == E_OK) {
-          compare_success = memcmp(&ctx->section_header, ctx->cmd_write_verify, ECU_FLASH_SECTION_HEADER_LENGTH) == 0;
-          if(compare_success == true) {
-            ctx->cmd_left_len -= ECU_FLASH_SECTION_HEADER_LENGTH;
-            ctx->cmd_cur_len -= ECU_FLASH_SECTION_HEADER_LENGTH;
-            ctx->cmd_address += ECU_FLASH_SECTION_HEADER_LENGTH;
-            ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD;
-          } else {
-            err = E_BADRESP;
-            ctx->cmd_errcode_internal = err;
-            ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
-          }
+          ctx->cmd_left_len -= ECU_FLASH_SECTION_HEADER_LENGTH;
+          ctx->cmd_cur_len -= ECU_FLASH_SECTION_HEADER_LENGTH;
+          ctx->cmd_address += ECU_FLASH_SECTION_HEADER_LENGTH;
+          ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD;
           continue;
         } else if(err != E_AGAIN) {
           ctx->cmd_errcode_internal = err;
@@ -380,41 +352,14 @@ error_t flash_fsm(flash_runtime_ctx_t *ctx)
       case FLASH_FSM_WRITE_PAYLOAD_SYNC:
         err = qspi_sync(ctx->qspi_ctx);
         if(err == E_OK) {
-          ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD_VERIFY;
-          continue;
-        } else if(err != E_AGAIN) {
-          ctx->cmd_errcode_internal = err;
-          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
-        }
-        break;
-      case FLASH_FSM_WRITE_PAYLOAD_VERIFY:
-        err = qspi_fast_read(ctx->qspi_ctx, ctx->cmd_address, ctx->cmd_write_verify, ctx->cmd_cur_len);
-        if(err == E_OK) {
-          ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD_VERIFY_SYNC;
-          continue;
-        } else if(err != E_AGAIN) {
-          ctx->cmd_errcode_internal = err;
-          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
-        }
-        break;
-      case FLASH_FSM_WRITE_PAYLOAD_VERIFY_SYNC:
-        err = qspi_sync(ctx->qspi_ctx);
-        if(err == E_OK) {
-          compare_success = memcmp(ctx->cmd_payload_tx_temp, ctx->cmd_write_verify, ctx->cmd_cur_len) == 0;
-          if(compare_success == true) {
-            ctx->cmd_payload_tx_temp += ctx->cmd_cur_len;
-            ctx->cmd_address += ctx->cmd_cur_len;
-            ctx->cmd_left_len -= ctx->cmd_cur_len;
-            ctx->cmd_cur_len = ctx->cmd_left_len > ctx->qspi_ctx->init.prog_page_size ? ctx->qspi_ctx->init.prog_page_size : ctx->cmd_left_len;
-            if(ctx->cmd_cur_len > 0) {
-              ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD;
-            } else {
-              ctx->fsm_process = FLASH_FSM_SECTION_ADDR;
-            }
+          ctx->cmd_payload_tx_temp += ctx->cmd_cur_len;
+          ctx->cmd_address += ctx->cmd_cur_len;
+          ctx->cmd_left_len -= ctx->cmd_cur_len;
+          ctx->cmd_cur_len = ctx->cmd_left_len > ctx->qspi_ctx->init.prog_page_size ? ctx->qspi_ctx->init.prog_page_size : ctx->cmd_left_len;
+          if(ctx->cmd_cur_len > 0) {
+            ctx->fsm_process = FLASH_FSM_WRITE_PAYLOAD;
           } else {
-            err = E_BADRESP;
-            ctx->cmd_errcode_internal = err;
-            ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
+            ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_ENABLE;
           }
           continue;
         } else if(err != E_AGAIN) {
@@ -422,7 +367,90 @@ error_t flash_fsm(flash_runtime_ctx_t *ctx)
           ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
         }
         break;
-
+      case FLASH_FSM_WRITE_MEMMAP_ENABLE:
+        err = qspi_memory_mapping_set(ctx->qspi_ctx, true);
+        if(err == E_OK) {
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_LOCK;
+          continue;
+        } else if(err != E_AGAIN) {
+          ctx->cmd_errcode_internal = err;
+          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
+        }
+        break;
+      case FLASH_FSM_WRITE_MEMMAP_CRC_LOCK:
+        err = crc_lock();
+        if(err == E_OK) {
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_HEADER;
+          ctx->cmd_address = ctx->cmd_address_base;
+          ctx->cmd_left_len = ECU_FLASH_SECTION_HEADER_LENGTH;
+          continue;
+        } else if(err != E_AGAIN) {
+          ctx->cmd_errcode_internal = err;
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_DISABLE;
+        }
+        break;
+      case FLASH_FSM_WRITE_MEMMAP_CRC_HEADER:
+        err = crc_calculate(&ctx->checksum, (void *)(ctx->qspi_ctx->init.memory_base_address + ctx->cmd_address), OFFSETOF(flash_section_header_t, crc), true);
+        if(err == E_OK) {
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_PAYLOAD;
+          ctx->cmd_address += ECU_FLASH_SECTION_HEADER_LENGTH;
+          ctx->cmd_left_len = ctx->cmd_length;
+          continue;
+        } else if(err != E_AGAIN) {
+          ctx->cmd_errcode_internal = err;
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_UNLOCK;
+        }
+        break;
+      case FLASH_FSM_WRITE_MEMMAP_CRC_PAYLOAD:
+        err = crc_calculate(&ctx->checksum, (void *)(ctx->qspi_ctx->init.memory_base_address + ctx->cmd_address), ctx->cmd_length, false);
+        if(err == E_OK) {
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_UNLOCK;
+          ctx->cmd_left_len = 0;
+          continue;
+        } else if(err != E_AGAIN) {
+          ctx->cmd_errcode_internal = err;
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_CRC_UNLOCK;
+          continue;
+        }
+        break;
+      case FLASH_FSM_WRITE_MEMMAP_CRC_UNLOCK:
+        err = crc_unlock();
+        if(err == E_OK) {
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_DISABLE;
+          continue;
+        } else if(err != E_AGAIN) {
+          if(ctx->cmd_errcode_internal != E_AGAIN) {
+            ctx->cmd_errcode_internal = err;
+          }
+          ctx->fsm_process = FLASH_FSM_WRITE_MEMMAP_DISABLE;
+          continue;
+        }
+        break;
+      case FLASH_FSM_WRITE_MEMMAP_DISABLE:
+        err = qspi_memory_mapping_set(ctx->qspi_ctx, false);
+        if(err == E_OK) {
+          if(ctx->cmd_errcode_internal != E_AGAIN) {
+            ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
+          } else {
+            ctx->fsm_process = FLASH_FSM_WRITE_CHECKSUM_VERIFY;
+          }
+          continue;
+        } else if(err != E_AGAIN) {
+          if(ctx->cmd_errcode_internal != E_AGAIN) {
+            ctx->cmd_errcode_internal = err;
+          }
+          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
+          continue;
+        }
+        break;
+      case FLASH_FSM_WRITE_CHECKSUM_VERIFY:
+        if(ctx->checksum == ctx->section_header.crc) {
+          ctx->fsm_process = FLASH_FSM_SECTION_ADDR;
+        } else {
+          ctx->cmd_errcode_internal = err;
+          ctx->fsm_process = FLASH_FSM_FLASH_UNLOCK;
+        }
+        continue;
       case FLASH_FSM_FLASH_UNLOCK:
         err = qspi_unlock(ctx->qspi_ctx);
         if(err != E_AGAIN) {
